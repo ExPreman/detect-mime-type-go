80c3fc3b34c99e4f297570da61f4a00c4a91b264
Yusuf Septiananda <septiananday@gmail.com>
2018-12-21 17:29:27 +0700

    add package

--- main.go
+++ main.go
@@ -2,40 +2,22 @@ package main
 
 import (
 	"fmt"
-	"net/http"
+	"io/ioutil"
 	"os"
+
+	"github.com/gabriel-vasile/mimetype"
 )
 
 func main() {
 	// Open File
-	f, err := os.Open("test_atuh.docx")
+	f, err := os.Open("crut.docx")
 	if err != nil {
 		panic(err)
 	}
 	defer f.Close()
 
 	// Get the content
-	contentType, err := GetFileContentType(f)
-	if err != nil {
-		panic(err)
-	}
-
-	fmt.Println("Content Type: " + contentType)
-}
-
-func GetFileContentType(out *os.File) (string, error) {
-
-	// Only the first 512 bytes are used to sniff the content type.
-	buffer := make([]byte, 512)
-
-	_, err := out.Read(buffer)
-	if err != nil {
-		return "", err
-	}
-
-	// Use the net/http package's handy DectectContentType function. Always returns a valid
-	// content-type by returning "application/octet-stream" if no others seemed to match.
-	contentType := http.DetectContentType(buffer)
-
-	return contentType, nil
+	buf, _ := ioutil.ReadFile("crut.docx")
+	mime, extension := mimetype.Detect([]byte(buf))
+	fmt.Println("Mime: ", mime, "Extension: ", extension)
 }
